2703f5cc57858586c0d26dd70a4f656ad13130ee
Italo-990 <italo.ribeiro2605@gmail.com>
2023-03-13 10:21:28 -0100

    Copia-Twitter

---
+++ JS/jsIndex.js
@@ -0,0 +1,20 @@
+const accountPublicInf = document.querySelectorAll('.accountPublicInf')
+const svgInput = document.querySelector('.svgInput')
+const inputSearch = document.querySelector('.inputSearch')
+const spanInputSearch = document.querySelector('.spanInputSearch')
+for(let i = 0; i < accountPublicInf.length; i++){
+    if(accountPublicInf[i].href == window.location.href){
+        accountPublicInf[i].classList.add('linkActive')
+    }
+
+}
+inputSearch.addEventListener('focus',function(){
+    svgInput.classList.add('inputFocusSvg')
+    inputSearch.classList.add('inputFocus')
+    spanInputSearch.classList.add('inputFocusSpan')
+})
+inputSearch.addEventListener('focusout',function(){
+    svgInput.classList.remove('inputFocusSvg')
+    inputSearch.classList.remove('inputFocus')
+    spanInputSearch.classList.remove('inputFocusSpan')
+})
