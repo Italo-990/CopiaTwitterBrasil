35ee8f99770bee4df7f0eddfa075ae23dbf37da6
Ítalo Maia <italo.ribeiro2605@gmail.com>
2023-06-07 10:35:25 +0000

    Update jsIndex.js
    remove classLink primeiro ao abrir

--- JS/jsIndex.js
+++ JS/jsIndex.js
@@ -2,12 +2,8 @@ const accountPublicInf = document.querySelectorAll('.accountPublicInf')
 const svgInput = document.querySelector('.svgInput')
 const inputSearch = document.querySelector('.inputSearch')
 const spanInputSearch = document.querySelector('.spanInputSearch')
-accountPublicInf[0].classList.add('linkActive');
 for(let i = 0; i < accountPublicInf.length; i++){
     if(accountPublicInf[i].href == window.location.href){
-        if(accountPublicInf[0].classList.contains('linkActive'){
-               accountPublicInf[0].classList.remove('linkActive')
-           }
         accountPublicInf[i].classList.add('linkActive')
     }
 
